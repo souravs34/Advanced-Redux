397b3f75e1bec23f408d990fc76896c7395e9370
Sourav34 <borateourav@gmail.com>
2021-10-11 13:32:35 +0530

    Basic Shopping Cart done

--- src/components/Shop/Products.js
+++ src/components/Shop/Products.js
@@ -1,16 +1,46 @@
-import ProductItem from './ProductItem';
-import classes from './Products.module.css';
+import ProductItem from "./ProductItem";
+import classes from "./Products.module.css";
 
+const DUMMY_PRODUCTS = [
+  {
+    id: "p1",
+    price: 6,
+    title: "My First Book",
+    description: "The first book I ever wrote",
+  },
+  {
+    id: "p2",
+    price: 12,
+    title: "My Second Book",
+    description: "The Second book I ever wrote",
+  },
+  {
+    id: "p3",
+    price: 18,
+    title: "My Third Book",
+    description: "The Third book I ever wrote",
+  },
+  {
+    id: "p4",
+    price: 24,
+    title: "My Fourth Book",
+    description: "The fourth book I ever wrote",
+  },
+];
 const Products = (props) => {
   return (
     <section className={classes.products}>
       <h2>Buy your favorite products</h2>
       <ul>
-        <ProductItem
-          title='Test'
-          price={6}
-          description='This is a first product - amazing!'
-        />
+        {DUMMY_PRODUCTS.map((product) => (
+          <ProductItem
+            id={product.id}
+            key={product.id}
+            title={product.title}
+            price={product.price}
+            description={product.description}
+          />
+        ))}
       </ul>
     </section>
   );
